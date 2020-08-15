2d5d105bd28cd43a9c4147817be5c38b9a124619
shashwat3115 <shashwat3115@gmail.com>
2020-08-15 17:42:28 +0530

    ujidhbgijkjurik

---
+++ screens/HomeScreen.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Button } from 'react-native';
+import { Header, Icon } from 'react-native-elements';
+//Importing external library
+import * as Speech from 'expo-speech';
+
+import dictionary from '../database';
+
+export default class HomeScreen extends Component{
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      isSearchPressed: false,
+      isLoading: false,
+      word  : "Please wait while we fetch the word",
+      lexicalCategory :'',
+      definition : ""
+    };
+  }
+
+  onSpeak = ()=>{
+    Speech.speak(this.state.word,{
+      language : 'en',
+      pitch : 1,
+      rate : 1
+    })
+  }
+
+  getWord=(text)=>{
+    var text = text.toLowerCase()
+    try{
+      var word = dictionary[text]["word"]
+      var lexicalCategory = dictionary[text]["lexicalCategory"]
+      var definition = dictionary[text]["definition"]
+      this.setState({
+        "word" : word,
+        "lexicalCategory" : lexicalCategory,
+        "definition" : definition
+      })
+    }
+    catch(err){
+      alert("Sorry, this word is not avaliable, please check your spelling or try again!")
+      this.setState({
+        'text':'',
+        'isSearchPressed':false
+      })
+    }
+  }
+
+  render(){
+    return(
+      <View style={{flex:1, backgroundColor:'powderblue'}}>
+        <Header
+          backgroundColor={'crimson'}
+          centerComponent={{
+            text: 'Pocket Dictionary',
+            style: { color: '#fff', fontSize: 20 },
+          }}
+        />
+        <View style={styles.inputBoxContainer}>
+          <TextInput
+            style={styles.inputBox}
+            onChangeText={text => {
+              this.setState({
+                text: text,
+                isSearchPressed: false,
+                word  : "Please wait while we fetch the word",
+                lexicalCategory :'',
+                examples : [],
+                defination : ""
+              });
+            }}
+            value={this.state.text}
+          />
+
+          <TouchableOpacity
+            style={styles.searchButton}
+            onPress={() => {
+              this.setState({ isSearchPressed: true });
+              this.getWord(this.state.text)
+            }}>
+            <Text style={styles.searchText}>Search</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.outputContainer}>
+          <Text style={{fontSize:20}}>
+            {
+              this.state.isSearchPressed && this.state.word === "Please wait while we fetch the word"
+              ? this.state.word
+              : ""
+            }
+          </Text>
+            {
+              this.state.word !== "Please wait while we fetch the word" ?
+              (
+                <View style={{justifyContent:'center', marginLeft:10 }}>
+                  <View style={styles.detailsContainer}>
+                    <Text style={styles.detailsTitle}>
+                      Word :{" "}
+                    </Text>
+                    <Text style={{fontSize:18 }}>
+                      {this.state.word}
+                    </Text>
+                  </View>
+                  <View style={styles.detailsContainer}>
+                    <Text style={styles.detailsTitle}>
+                      Type :{" "}
+                    </Text>
+                    <Text style={{fontSize:18}}>
+                      {this.state.lexicalCategory}
+                    </Text>
+                  </View>
+                  <View style={{flexDirection:'row',flexWrap: 'wrap'}}>
+                    <Text style={styles.detailsTitle}>
+                      Definition :{" "}
+                    </Text>
+                    <Text style={{ fontSize:18}}>
+                      {this.state.definition}
+                    </Text>
+                  </View>
+                  <View
+                    style={{justifyContent:'center',alignItems:'center', marginTop:10, flexDirection:'row'}}
+                    >
+                    <Icon
+                      name='sound'
+                      type='antdesign'
+                      color='black'
+                    />
+                    <TouchableOpacity
+                      onPress={this.onSpeak}
+                      style={styles.speechButton}
+                    >
+                      <Text>{this.state.text}</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )
+              :null
+            }
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputBoxContainer: {
+    flex:0.3,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  inputBox: {
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+  },
+  searchButton: {
+    width: '40%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    backgroundColor:'lightgreen',
+    borderWidth: 2,
+    borderRadius: 10,
+
+  },
+  searchText:{
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  outputContainer:{
+    flex:0.7,
+    alignItems:'center'
+  },
+  detailsContainer:{
+    flexDirection:'row',
+    alignItems:'center'
+  },
+  detailsTitle:{
+    color:'orange',
+    fontSize:20,
+    fontWeight:'bold'
+  },
+  speechButton:{
+    width:200,
+    height:40,
+    justifyContent:'center',
+    alignItems:'center',
+    borderWidth:2,
+    backgroundColor:'orange',
+    borderRadius:10,
+    marginLeft:5
+  }
+});
